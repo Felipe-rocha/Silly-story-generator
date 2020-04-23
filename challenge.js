f49954e19192072f5ece03bc15aa0a60c5d2edce
karolinny <karolinny.silva@gmail.com>
2020-04-22 22:29:25 -0300

    Story Text

--- challenge.js
+++ challenge.js
@@ -1,5 +1,15 @@
 var historia = document.querySelector(".randomize");
+var story = document.querySelector(".story");
 
 historia.addEventListener("click", function(){
     console.log("Fui clicado");
+    History();
+
 });
+
+function History(){
+    storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.".replace("insertx", "KAROLINNY");
+    console.log(storyText);
+    story.innerHTML = storyText;
+    story.style.visibility = 'visible';
+}
