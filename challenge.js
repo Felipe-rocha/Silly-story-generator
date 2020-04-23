868dbc6eb655b5b0a1126d0e72fae006ca23dd76
Felipe Rocha <64161583+Felipe-rocha@users.noreply.github.com>
2020-04-23 11:32:21 -0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,15 +1,17 @@
-var historia = document.querySelector(".randomize");
+var geraHistoria = document.querySelector(".randomize");
 var story = document.querySelector(".story");
+var nomeInput = document.getElementById("customname").value;
 
-historia.addEventListener("click", function(){
-    console.log("Fui clicado");
+geraHistoria.addEventListener("click", function(){
     History();
-
+    console.log(nomeInput);
 });
 
+
 function History(){
-    storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.".replace("insertx", "KAROLINNY");
-    console.log(storyText);
+    
+    storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.".replace(":insertx:", nomeInput);
     story.innerHTML = storyText;
     story.style.visibility = 'visible';
+    
 }
