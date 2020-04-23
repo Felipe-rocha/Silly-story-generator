77286f77dcc0f024ac36f79ff720b83265b9cfa5
Felipe Rocha <64161583+Felipe-rocha@users.noreply.github.com>
2020-04-22 22:02:30 -0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,5 +1,5 @@
-var historia = document.querySelector("#randomize");
+var historia = document.querySelector(".randomize");
 
 historia.addEventListener("click", function(){
-    console.log("Fui clicado!");
+    console.log("Fui clicado");
 });
