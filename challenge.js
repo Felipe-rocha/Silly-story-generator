34a097949a3d9c2cb18c9a4c31356628c81f2dee
Felipe Rocha <64161583+Felipe-rocha@users.noreply.github.com>
2020-04-24 18:03:49 -0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,17 +1,22 @@
 var geraHistoria = document.querySelector(".randomize");
 var story = document.querySelector(".story");
-var nomeInput = document.getElementById("customname").value;
 
 geraHistoria.addEventListener("click", function(){
     History();
-    console.log(nomeInput);
+    name(storyText);
 });
 
-
-function History(){
-    
-    storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.".replace(":insertx:", nomeInput);
+function History(nomeInput){
+    storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
     story.innerHTML = storyText;
     story.style.visibility = 'visible';
-    
+}
+
+function name(storyText){
+    var nomeInput = document.getElementById("customname").value;
+    var trocaNome = storyText;
+    trocaNome.replace("Bob", nomeInput.value);
+
+    console.log(trocaNome);
+    console.log(nomeInput);
 }
