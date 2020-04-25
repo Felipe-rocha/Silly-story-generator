402f607d2246d7f8de6812335a4509764b0e0d91
Felipe Rocha <64161583+Felipe-rocha@users.noreply.github.com>
2020-04-24 21:52:32 -0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -3,12 +3,10 @@ var story = document.querySelector(".story");
 
 geraHistoria.addEventListener("click", function(){
     History();
-    name(storyText);
-    alteraTexto();
 });
 
 function History(){
-    // Essa função, até o storyText, só funciona quando é executada dentro da função History. Sempre que tento abrir uma função para ela, as variáveis mudaTextoX, mudaTextoY e mudaTextoZ não são reconhecidas no texto.
+    // Essas funções só funcionam quando são executados dentro da função History. Sempre que tento abrir uma função externa, as variáveis não são reconhecidas, ficam como "undefined".
     var randomInsertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
     var randomInsertY = ["the soup kitchen", "Disneyland", "the White House"];
     var randomInsertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
@@ -21,17 +19,16 @@ function History(){
     var mudaTextoY = randomInsertY[inserty];
     var mudaTextoZ = randomInsertZ[insertz];
 
-    storyText = "It was 94 fahrenheit outside, so " + mudaTextoX + " went for a walk. When they got to " + mudaTextoY + " , they stared in horror for a few moments, then " + mudaTextoZ + ". Bob saw the whole thing, but was not surprised — " + mudaTextoX + " weighs 300 pounds, and it was a hot day.";
+    var nomeInput = document.getElementById("customname").value;   
+    
+    if(nomeInput.length > 0){
+        var trocaNome = nomeInput;
+    }else{
+        var trocaNome = "Bob";
+    }
+
+    storyText = "It was 94 fahrenheit outside, so " + mudaTextoX + " went for a walk. When they got to " + mudaTextoY + " , they stared in horror for a few moments, then " + mudaTextoZ + ". " + trocaNome + " saw the whole thing, but was not surprised — " + mudaTextoX + " weighs 300 pounds, and it was a hot day.";
     story.innerHTML = storyText;
     story.style.visibility = 'visible';
+
 }
-
-function name(storyText){
-    var nomeInput = document.getElementById("customname").value;
-    var trocaNome = storyText;
-    trocaNome.replace("Bob", "felipe");
-
-    console.log(trocaNome);
-    console.log(nomeInput);
-}
-
